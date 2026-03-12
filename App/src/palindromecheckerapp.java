b00aa4b23e9a0ffbe3850a9a30292633e5f4634a
arulnithish281-ux <arulnithish281@gmail.com>
2026-03-12 14:30:01 +0530

    palindromecheckerApp

---
+++ App/src/palindromecheckerapp.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class palindromecheckerapp {
+
+
+    public class PalindromeChecker {
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            String a = sc.nextLine();
+
+            int i = 0;
+            int l = a.length();
+            boolean flag = true;
+
+            while (i < l / 2) {
+                if (a.charAt(i) != a.charAt(l - i - 1)) {
+                    flag = false;
+                    break;
+                }
+                i++;
+            }
+
+            if (flag)
+                System.out.println("Is a Palindrome");
+            else
+                System.out.println("Not a Palindrome");
+        }
+    }
+}
